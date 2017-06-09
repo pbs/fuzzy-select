2df4490cf48a192a2dda8d61945d270c76d83835
Chip Bell <cbell@pbs.org>
2017-06-09 11:31:50 -0400

    Updating example to use the iterative method

--- app.js
+++ app.js
@@ -13,10 +13,14 @@ var makeColorGrid = function(size) {
   return new ImageDataColorGrid(canvas.getContext('2d').getImageData(0, 0, size, size));
 }
 
+setStatus('Making color grid');
+var colorGrid = makeColorGrid(5000);
+setStatus('Ready.');
 
 document.querySelector('#fuzzy-select').addEventListener('click', function() {
-  setStatus('Making color grid');
-  var colorGrid = makeColorGrid(5000);
+  var button = this;
+  button.disabled = true;
+
   var selector = new FuzzySelector(colorGrid);
 
   setStatus('Selecting');
@@ -24,9 +28,24 @@ document.querySelector('#fuzzy-select').addEventListener('click', function() {
   var t1 = Date.now();
 
   // select a region
-  var selectedRegion = selector.select(1, 0, 20);
+  var generator = selector.selectIteratively(1, 0, 20);
+  var stepsPerFrame = 10;
+  var doSteps = function() {
+    var i = 0, current = { done: false, value: undefined };
+    while(i < stepsPerFrame && !current.done) {
+      current = generator.next();
+      i++;
+    }
 
-  var t2 = Date.now();
+    if(current.done) {
+      var t2 = Date.now();
+      setStatus('Selected in ' + (t2 - t1) + 'ms');
+      button.disabled = false;
+      return;
+    }
 
-  setStatus('Selected in ' + (t2 - t1) + 'ms');
+    requestAnimationFrame(doSteps);
+  };
+
+  requestAnimationFrame(doSteps);
 });
