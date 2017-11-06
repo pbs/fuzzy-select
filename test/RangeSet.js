463b7932073151bbee2d3e69c58610f9efb950f8
Chip Bell <cbell@pbs.org>
2017-11-06 15:25:19 -0500

    Getting rid of some duplication

--- test/RangeSet.js
+++ test/RangeSet.js
@@ -3,9 +3,13 @@ var Range = require('../Range');
 var RangeSet = require('../RangeSet');
 
 describe('RangeSet', function() {
+  var set;
+  beforeEach(function() {
+    set = new RangeSet();
+  });
+
   describe('add', function() {
     it('should properly add a new range under the appropriate key', function() {
-      var set = new RangeSet();
       var range = new Range(0, 10);
       set.add(range, 2);
 
@@ -15,18 +19,15 @@ describe('RangeSet', function() {
 
   describe('contains', function() {
     it('should return false if the range set does not contain any entry for that y coordinate', function() {
-      var set = new RangeSet();
       expect(set.contains(1, 2)).to.equal(false);
     });
 
     it('should return false if the range set has an entry for that y, but it does not contain that number', function() {
-      var set = new RangeSet();
       set.add(new Range(0, 1), 2);
       expect(set.contains(2, 2)).to.equal(false);
     });
 
     it('should return true if the cell is contained in the set', function() {
-      var set = new RangeSet();
       set.add(new Range(0, 1), 2);
       expect(set.contains(2, 1)).to.equal(true);
     });
@@ -34,7 +35,6 @@ describe('RangeSet', function() {
 
   describe('boundaries', function() {
     it('should return a cell set of boundaries', function() {
-      var set = new RangeSet();
       set.add(new Range(0, 1), 2);
       set.add(new Range(1, 2), 1);
       var cells = set.boundaries().cells;
@@ -44,7 +44,6 @@ describe('RangeSet', function() {
 
   describe('toPathString', function() {
     it('should return a valid SVG path string matching the provided set', function() {
-      var set = new RangeSet();
       set.add(new Range(1, 10), 2);
       set.add(new Range(2, 5), 3);
       var path = set.toPathString();
